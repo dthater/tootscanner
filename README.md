f87b4de3ee81d7622cce4a791545e5b1d12e9866
Damian Thater <info@damianthater.com>
2022-11-19 23:26:51 +0100

    Format results to conform CSV import file format.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Twitter Crawler for Mastodon handles
 
-This nice JavaScript script scans your follower lists on Twitter for Mastodon handles.
+This handy JavaScript script scans your follower lists on Twitter for Mastodon handles.
 
-No access granting of foreign sites is neccessary. 
+No access granting of foreign sites is neccessary.
 No private information leaves your browser.
 
 ## How to use
@@ -10,7 +10,7 @@ No private information leaves your browser.
 1. Log into Twitter in Chrome.
 2. Navigate to your follower list.
 3. Open the JavaScript console (F12)
-4. Copy the contents of mastodon-handle-crawler.js into your console.
+4. Copy the contents of mastodon-handle-crawler.js into the console.
 5. Run by hitting enter.
 
 The script will start scanning for Mastodon handles in the bios and the Twitter name.
@@ -18,5 +18,10 @@ The script will start scanning for Mastodon handles in the bios and the Twitter 
 Scroll down to load the chunk of followers.
 The script will automatically fill the result list, the yellow box on the left.
 
-To automate scrolling execute `startScrolling()` in the console. 
+To automate scrolling execute `startScrolling()` in the console.
 To stop scrolling, execute `stopScrolling()`.
+
+While scanning, URLs will be formated to conform @name@host format.
+
+The script produces a csv list of handles, ready to copy and past into a empty csv-file.
+You can import this csv file into your Mastodon account.
--- mastodon-handle-crawler.js
+++ mastodon-handle-crawler.js
@@ -2,64 +2,95 @@ let tootlist = new Set()
 let lastAmountScanned = 0
 
 let tootresult = document.getElementById("tootresult")
-if (!tootresult){
-	tootresult = document.createElement("pre")
-	tootresult.id = "tootresult"
-	document.body.insertBefore(tootresult, null)
-	tootresult.style.background = "yellow"
-	tootresult.style.position = "fixed"
-	tootresult.style.left = 0
-	tootresult.style.top = 0
-	tootresult.style.bottom = 0
-	tootresult.style.padding = ".5em"
-	tootresult.style.scrollBar = "auto"
-	tootresult.style.overflowY = "scroll"  
+if (!tootresult) {
+  tootresult = document.createElement("textarea")
+  tootresult.id = "tootresult"
+  document.body.insertBefore(tootresult, null)
+  tootresult.style.background = "yellow"
+  tootresult.style.position = "fixed"
+  tootresult.style.left = 0
+  tootresult.style.top = 0
+  tootresult.style.bottom = 0
+  tootresult.style.width = "20%"
+  tootresult.style.padding = ".5em"
+  tootresult.style.overflowY = "scroll"
+  tootresult.style.whiteSpace = "pre"
 }
 
-function updateResults(){
-  tootresult.innerText = `${tootlist.size} Mastodon handles:\n${[...tootlist].join("\n")}`
+function updateResults() {
+  console.log(`${tootlist.size} Mastodon handles found.`)
+  tootresult.value =
+    printCsvHeader() +
+    printCsvBody([...tootlist])
 }
 
-function scanToots(){
-[...document.querySelectorAll("[data-testid='cellInnerDiv']")]
-.map( t => t.innerText )
-.map( t => {
-	let arr = [
-		/(@[a-zA-Z0-9-]*@[a-zA-Z0-9-]*\.[a-zA-Z0-9-]*)/g,
-		/([a-zA-Z0-9\.:\/-]*\/@[a-zA-Z0-9-]*)/g,
-		/([a-zA-Z0-9-]*@[a-zA-Z0-9-]*\.social)/g
-	].map(k => k.exec(t)).filter(k => k != null).map(k => k[1])
-	return arr.length ? arr[0] : null
-})
-.filter( t => t != null).forEach( t => {
-	let prevAmountScanned = lastAmountScanned
-  tootlist.add(t)
-  lastAmountScanned = tootlist.size
-  if (prevAmountScanned != lastAmountScanned) {
-    updateResults()
+function printCsvHeader() {
+  return "Account address,Show boosts,Notify on new posts,Languages\n"
+}
+
+function printCsvBody(list) {
+  return list.map(t => `${t},true,false,`).join("\n")
+}
+
+function scanToots() {
+  [...document.querySelectorAll("[data-testid='cellInnerDiv']")]
+    .map(t => t.innerText)
+    .map(findHandle)
+    .filter(t => t != null)
+    .map(convertUrlToHandle)
+    .map(fixHandle)
+    .filter(t => t != null)
+    .forEach(t => {
+      let prevAmountScanned = lastAmountScanned
+      tootlist.add(t)
+      lastAmountScanned = tootlist.size
+      if (prevAmountScanned != lastAmountScanned) {
+        updateResults()
+      }
+    })
+}
+
+function findHandle(text) {
+  let arr = [
+    /(@[a-zA-Z0-9-]*@[a-zA-Z0-9-]*\.[a-zA-Z0-9-]*)/g,
+    /([a-zA-Z0-9\.:\/-]*\/@[a-zA-Z0-9-]*)/g,
+    /([a-zA-Z0-9-]*@[a-zA-Z0-9-]*\.social)/g
+  ].map(k => k.exec(text)).filter(k => k != null).map(k => k[1])
+  return arr.length ? arr[0] : null
+}
+
+function convertUrlToHandle(handle) {
+  let items = /(http[s]*:\/\/)([a-zA-Z0-9\.-\/]*)\/(@[a-zA-Z0-9-]*)/g.exec(handle)
+  if (items) {
+    let [finding, protocoll, host, name] = items
+    return `${name}@${host}`
   }
-})
+  return handle
 }
 
-function startScanner(){
+function fixHandle(handle) {
+  return handle.indexOf("@") != 0 ? `@${handle}` : handle
+}
+
+function startScanner() {
   stopScanner()
   self.hTootScanner = setInterval(scanToots, 250)
 }
 
-function stopScanner(){
+function stopScanner() {
   if (self.hTootScanner) {
     clearInterval(self.hTootScanner)
   }
 }
 
-function startScrolling(){
+function startScrolling() {
   stopScrolling()
-  self.hTootScroller = setInterval(function (){
-    [...document.querySelectorAll("[data-testid='cellInnerDiv']")].filter( (a,b,c) => b == c.length-1 ).reduce( (d,v) => v, null ).scrollIntoView()
+  self.hTootScroller = setInterval(function () {
+    [...document.querySelectorAll("[data-testid='cellInnerDiv']")].filter((a, b, c) => b == c.length - 1).reduce((d, v) => v, null).scrollIntoView()
   }, 250)
 }
 
-function stopScrolling(){
+function stopScrolling() {
   if (self.hTootScroller) {
     clearInterval(self.hTootScroller)
   }
